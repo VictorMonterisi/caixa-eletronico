aa13a1d959ae0be7e621c65e725ec19bacdd53d5
Victor Monterisi <victoraugustobm1@gmail.com>
2023-11-10 18:08:49 -0300

    ajustes JSON Vercel

--- api/assets/php/includes/head.php
+++ api/assets/php/includes/head.php
@@ -9,9 +9,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <?php
-    require "assets/php/Classes/Cliente.php";
-    require "assets/php/Classes/Operacoes.php";
-    require "assets/php/Classes/Relogio.php";
+    $url = 'https://meu-caixa-eletronico.vercel.app/api/';
+    require "{$url}assets/php/Classes/Cliente.php";
+    require "{$url}assets/php/Classes/Operacoes.php";
+    require "{$url}assets/php/Classes/Relogio.php";
 
     $pega_dados = new Operacoes();
     $tempo = new Relogio();
